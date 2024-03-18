4ba08ebd8d37f8e1b295936acb2c4dd564d7cf93
WuLianN <610575024@qq.com>
2024-03-18 11:17:18 +0800

    fix: select字段错误，标签id没有返回

--- internal/dao/sys_tags.go
+++ internal/dao/sys_tags.go
@@ -42,13 +42,13 @@ func (d *Dao) QueryDraftTagsDT(userId uint32, tagId uint32) ([]model.DraftTag, e
 	var err error
 
 	if tagId > 0 {
-		d.engine.Table("draft_tags").Select("tags.id as id, draft_tags.draft_id, tags.name, tags.bg_color, tags.color").Joins("left join tags on draft_tags.tag_id = tags.id").Where("tags.user_id = ? AND draft_tags.tag_id = ?", userId, tagId).Find(&list)
+		d.engine.Table("draft_tags").Select("tags.id as tag_id, draft_tags.draft_id, tags.name, tags.bg_color, tags.color").Joins("left join tags on draft_tags.tag_id = tags.id").Where("tags.user_id = ? AND draft_tags.tag_id = ?", userId, tagId).Find(&list)
 
 		return list, nil
 	}
 
 	if userId > 0 {
-		d.engine.Table("draft_tags").Select("tags.id as id, draft_tags.draft_id, tags.name, tags.bg_color, tags.color").Joins("left join tags on draft_tags.tag_id = tags.id").Where("tags.user_id = ?", userId).Find(&list)
+		d.engine.Table("draft_tags").Select("tags.id as tag_id, draft_tags.draft_id, tags.name, tags.bg_color, tags.color").Joins("left join tags on draft_tags.tag_id = tags.id").Where("tags.user_id = ?", userId).Find(&list)
 
 		return list, nil
 	}
